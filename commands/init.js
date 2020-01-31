17f4fc166f8d4cd659ce2b2c1157d0b977a35caa
lizhi <lizhi@camelotchina.com>
2020-02-01 00:40:24 +0800

    初始化

---
+++ commands/init.js
@@ -0,0 +1,93 @@
+
+const { prompt } = require('inquirer');
+const program = require('commander');
+const chalk = require('chalk');
+const download = require('download-git-repo');
+const ora = require('ora');
+const fs = require('fs');
+ 
+const option =  program.parse(process.argv).args[0];
+const question = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Project name',
+    default: typeof option === 'string' ? option : 'lz-ui-template',
+    filter (val) {
+      return val.trim()
+    },
+    validate (val) {
+      const validate = (val.trim().split(" ")).length === 1;
+      return validate || 'Project name is not allowed to have spaces ';
+    },
+    transformer (val) {
+      return val;
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Project description',
+    default: 'Vue project',
+    validate () {
+      return true;
+    },
+    transformer(val) {
+      return val;
+    }
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'Author',
+    default: '',
+    validate () {
+      return true;
+    },
+    transformer(val) {
+      return val;
+    }
+  }
+];
+ 
+module.exports = prompt(question).then(({name, description, author}) => {
+  const gitPlace = require('../templates').init.path;
+  const projectName = name;
+  const spinner = ora('Downloading please wait...');
+ 
+  spinner.start();
+  download(`${gitPlace}`, `./${projectName}`, (err) => {
+    if (err) {
+      console.log(chalk.red(err));
+      process.exit()
+    }
+ 
+    fs.readFile(`./${projectName}/package.json`, 'utf8', function (err, data) {
+      if(err) {
+        spinner.stop();
+        console.error(err);
+        return;
+      }
+ 
+      const packageJson = JSON.parse(data);
+      packageJson.name = name;
+      packageJson.description = description;
+      packageJson.author = author;
+ 
+      fs.writeFile(`./${projectName}/package.json`, JSON.stringify(packageJson, null, 2), 'utf8', function (err) {
+        if(err) {
+          spinner.stop();
+          console.error(err);
+        } else {
+          spinner.stop();
+          console.log(chalk.green('project init successfully!'))
+          console.log(`
+            ${chalk.yellow(`cd ${name}`)}
+            ${chalk.yellow('npm install')}
+            ${chalk.yellow('npm run dev')}
+          `);
+        }
+      });
+    });
+  })
+});
